a0804335de8440388a96626b2bc9ab8ea423c936
swbonjour <71038541+swbonjour@users.noreply.github.com>
2022-10-12 00:10:26 +0300

    about page

---
+++ src/components/MoreAbout/MoreAbout.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import "./FlagSvg"
+import FlagSvg from './FlagSvg'
+
+import "./MoreAbout.scss"
+
+export default function MoreAbout() {
+  return (
+    <div>
+        <main className="about">
+            <div className="about__content">
+                <p className="about__content__article">KNOW MORE ABOUT US</p>
+                <p className="about__content__title">We source sustainable & <br></br>line caught Foods</p>
+                <p className="about__content__additional">Edit this text to make it your own. To edit, simply click directly on the text to start adding your own words. You can move the text by dragging and dropping the text anywhere on the page.</p>
+                <div className="about__content__enumeration">
+                    <div className="about__content__enumeration__item">
+                        <p className="about__content__enumeration__item__header"><FlagSvg></FlagSvg>10+ People</p>
+                        <p className="about__content__enumeration__item__additional">We are Small Team</p>
+                        <p className="about__content__enumeration__item__description">Through True Rich Attended does no end it his mother since favourable.</p>
+                    </div>
+                    <div className="about__content__enumeration__item">
+                        <p className="about__content__enumeration__item__header"><FlagSvg></FlagSvg>2014</p>
+                        <p className="about__content__enumeration__item__additional">We are From</p>
+                        <p className="about__content__enumeration__item__description">Through True Rich Attended does no end it his mother since favourable.</p>
+                    </div>
+                    <div className="about__content__enumeration__item">
+                        <p className="about__content__enumeration__item__header"><FlagSvg></FlagSvg>200k</p>
+                        <p className="about__content__enumeration__item__additional">We Served</p>
+                        <p className="about__content__enumeration__item__description">Through True Rich Attended does no end it his mother since favourable.</p>
+                    </div>
+                </div>
+                <button className="about__content__menu">View Menu</button>
+            </div>
+            <div className="about__img">
+                <img src={require("./Know more Image 1.png")} alt="chairs" className="about__img__one"/>
+                <img src={require("./Know more Image 2.png")} alt="chairs" className="about__img__two"/>
+            </div>
+        </main>
+    </div>
+  )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Hero from '../../components/Hero/Hero'
+import MoreAbout from '../../components/MoreAbout/MoreAbout'
 
 import "./Home.scss"
 
@@ -9,6 +10,7 @@ export default function Home() {
     <div>
         <section className="home">
             <Hero></Hero>
+            <MoreAbout></MoreAbout>
         </section>
     </div>
   )

4a535a8b25e9274857f937358c6aab90a0897261
swbonjour <71038541+swbonjour@users.noreply.github.com>
2022-10-13 00:45:19 +0300

    serving

---
+++ src/components/Serving/Serving.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+import "./Serving.scss"
+
+import CakeSvg from './CakeSvg'
+import CoffeSvg from './CoffeSvg'
+import FoodSvg from './FoodSvg'
+import Cocktails from './Cocktails'
+import Grill from './Grill'
+import Snacks from './Snacks'
+
+export default function Serving() {
+  return (
+    <div>
+        <main className="serving">
+            <div className="serving__header">
+                <p className="serving__header__article">WHAT WE ARE SERVING</p>
+                <div className="serving__header__content">
+                    <p className="serving__header__content__title">We all have to eat, so why not <br></br>do it beautifully?</p>
+                    <p className="serving__header__content__additional">Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.. Through True Rich Attended does no end it his mother</p>
+                </div>
+            </div>
+            <div className="serving__content">
+                <div className="serving__content__item">
+                    <div className="serving__content__item__logo"><CakeSvg></CakeSvg></div>
+                    <p className="serving__content__item__title">Quafe Cake</p>
+                    <p className="serving__content__item__description">Through True Rich Attended no end it his mother since favourable real had half every him.</p>
+                </div>
+                <div className="serving__content__item">
+                    <div className="serving__content__item__logo"><CoffeSvg></CoffeSvg></div>
+                    <p className="serving__content__item__title">Cofee</p>
+                    <p className="serving__content__item__description">Through True Rich Attended no end it his mother since favourable real had half every him.</p>
+                </div>
+                <div className="serving__content__item">
+                    <div className="serving__content__item__logo"><FoodSvg></FoodSvg></div>
+                    <p className="serving__content__item__title">Fast Food</p>
+                    <p className="serving__content__item__description">Through True Rich Attended no end it his mother since favourable real had half every him.</p>
+                </div>
+                <div className="serving__content__item">
+                    <div className="serving__content__item__logo"><Cocktails></Cocktails></div>
+                    <p className="serving__content__item__title">Cocktails</p>
+                    <p className="serving__content__item__description">Through True Rich Attended no end it his mother since favourable real had half every him.</p>
+                </div>
+                <div className="serving__content__item">
+                    <div className="serving__content__item__logo"><Grill></Grill></div>
+                    <p className="serving__content__item__title">Grill</p>
+                    <p className="serving__content__item__description">Through True Rich Attended no end it his mother since favourable real had half every him.</p>
+                </div>
+                <div className="serving__content__item">
+                    <div className="serving__content__item__logo"><Snacks></Snacks></div>
+                    <p className="serving__content__item__title">Snacks</p>
+                    <p className="serving__content__item__description">Through True Rich Attended no end it his mother since favourable real had half every him.</p>
+                </div>
+            </div>
+        </main>
+    </div>
+  )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Hero from '../../components/Hero/Hero'
 import MoreAbout from '../../components/MoreAbout/MoreAbout'
+import Serving from '../../components/Serving/Serving'
 
 import "./Home.scss"
 
@@ -11,6 +12,7 @@ export default function Home() {
         <section className="home">
             <Hero></Hero>
             <MoreAbout></MoreAbout>
+            <Serving></Serving>
         </section>
     </div>
   )
